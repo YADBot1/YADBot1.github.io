d2c52d3390e8d682b1d797a979b0b16297318dc4
YADBot1 <162882435+YADBot1@users.noreply.github.com>
2024-03-10 20:43:35 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const progressBar = document.getElementById('progress-bar');
+    const progressText = document.getElementById('progress-text');
+    const completeButton = document.getElementById('complete-button');
+  
+    function showCompleteButton() {
+      completeButton.style.display = 'block';
+      progressBar.style.display = 'none';
+      progressText.style.display = 'none';
+    }
+  
+    let progressWidth = 0; // Початкове значення ширини прогресу
+    const animationDuration = 100000; // Тривалість анімації у мілісекундах (10 секунд)
+  
+    const animationStep = 100 / (animationDuration / 100); // Крок анімації для досягнення 100% за 10 секунд
+  
+    function animate() {
+      if (progressWidth < 100) {
+        progressWidth += animationStep;
+        progressBar.style.width = progressWidth + '%';
+        progressText.textContent = Math.round(progressWidth) + '%'; // Оновлення тексту з процентами
+        requestAnimationFrame(animate);
+      } else {
+        showCompleteButton();
+      }
+    }
+  
+    // Почати анімацію
+    animate();
+  });
+  
