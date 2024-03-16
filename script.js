b94347bb620467128ccd5dab1df6a8a4fc83bcf1
YADBot1 <162882435+YADBot1@users.noreply.github.com>
2024-03-16 11:15:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,61 +8,21 @@ document.addEventListener("DOMContentLoaded", function() {
         // Получаем значение параметра 'param1'
     var param1Value = searchParams.get('req');
 
-
-    const progressBar = document.getElementById('progress-bar');
-    const progressText = document.getElementById('progress-text');
-    const completeButton = document.getElementById('complete-button');
-  
-    function showCompleteButton() {
-      completeButton.style.display = 'block';
-      progressBar.style.display = 'none';
-      progressText.style.display = 'none';
-    }
-  
-    let progressWidth = 0; // Початкове значення ширини прогресу
-    const animationDuration = 25000; // Тривалість анімації у мілісекундах (10 секунд)
-  
-    const animationStep = 100 / (animationDuration / 100); // Крок анімації для досягнення 100% за 10 секунд
-  
-    function animate() {
-      if (progressWidth < 100) {
-        progressWidth += animationStep;
-        progressBar.style.width = progressWidth + '%';
-        progressText.textContent = Math.round(progressWidth) + '%'; // Оновлення тексту з процентами
-        requestAnimationFrame(animate);
-      } else {
-        showCompleteButton();
-      }
-    }
-  
+    let tg = window.Telegram.WebApp;
+    
     // Почати анімацію
     if(param1Value){
-        const concontainer = document.getElementById('container');
-        concontainer.style.display = "none";
-        animate();
+        tg.sendData(param1Value);
+        window.location.href = "https://www.profitablegatecpm.com/ftfx5mcg?key=affb32f08a6cec6b7611abc01504672a";
     }
     else{
-        const bottom_center = document.getElementById('bottom-center');
-        bottom_center.style.display = "none";
+      window.location.href = "https://www.profitablegatecpm.com/ftfx5mcg?key=affb32f08a6cec6b7611abc01504672a";
         
     }
     
 
 
-    const myButton = document.getElementById('complete-button');
-    let tg = window.Telegram.WebApp;
-    // Додаємо обробник подій для кліку на кнопку
-    myButton.addEventListener('click', function() {
-        // Получаем строку запроса из текущего URL
-        
-
-        try{
-            tg.sendData(param1Value);
-        } catch(e){
-            alert(e);
-        }
-        
-    });
+    
     
   }
   
